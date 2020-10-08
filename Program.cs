041e36e5edc452b5ee436b1a09fb27b9baffec53
Aryav <tiwarikaustav@gmail.com>
2020-10-08 10:17:39 +0530

    [Aryav] initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkShopOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame();
+        }
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkShopOne
+{
+    class TicTacToeGame
+    {
+        //creating an empty char array of size 10
+        static char[] arr = { '', '', '', '', '', '', '', '', '', '' };
+
+    }
+}
