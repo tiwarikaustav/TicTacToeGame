ff3e2d2418345447765417da02f7a36b962c1a37
Aryav <tiwarikaustav@gmail.com>
2020-10-08 11:22:18 +0530

    [Aryav] Added Code: Now the board can be displayed

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,8 +6,33 @@ namespace WorkShopOne
 {
     class TicTacToeGame
     {
-        //creating an empty char array of size 10
-        static char[] arr = { '', '', '', '', '', '', '', '', '', '' };
+        char [] board = new char[10];
+        public char[] CreateBoard()
+        {
+            //creating an empty char array of size 10
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = 'd';
+            }
+            return board;
+        }
 
+        //The Player Chooses X or O
+        public char ChooseXO()
+        {
+            char playerChoice;
+            Console.WriteLine("Enter Your Choice X/O");
+            playerChoice = Console.ReadKey().KeyChar;
+            return playerChoice;
+        }
+
+        //show the board
+        public void ShowBoard()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(board[1] + "|" + board[2] + "|" + board[3]);
+            Console.WriteLine(board[4] + "|" + board[5] + "|" + board[6]);
+            Console.WriteLine(board[7] + "|" + board[8] + "|" + board[9]);
+        }
     }
 }
